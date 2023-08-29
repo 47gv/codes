1c311afe367d36466e4ebbea7635aaf0a5423a0d
47gv <73207566+47gv@users.noreply.github.com>
2023-08-29 10:07:55 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games {'gta5'}
+description 'CamaleaoMods! https://discord.gg/dhufVUekb3'
+
+files{
+	'configs/**/*.meta',
+}
+
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'configs/**/weaponcomponents.meta'
+data_file 'WEAPON_METADATA_FILE' 'configs/**/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'configs/**/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' 'configs/**/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' 'configs/**/weapons.meta'
+
+client_script 'cl_weaponNames.lua'
+
+lua54 'yes'
